d938c6e316a385028b1de997ee53ca7289acbffc
Nikhil PP <nikhilpp@Nikhils-MacBook-Air.local>
2025-11-03 17:00:18 +0530

    Added contact and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { AboutSection } from "../components/AboutSection"
+import { ContactSection } from "../components/ContactSection"
+import { Footer } from "../components/Footer"
 import { HeroSection } from "../components/HeroSection"
 import { Navbar } from "../components/Navbar"
 import { ProjectSection } from "../components/ProjectSection"
@@ -24,9 +26,11 @@ export const Home = () => {
         <AboutSection />
         <SkillsSection />
         <ProjectSection />
+        <ContactSection />
       </main>
 
       {/* Simple Footer */}
+      <Footer />
     </div>
   )
 }
